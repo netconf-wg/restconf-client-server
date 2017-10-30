92413272f3470a8cba02803ef640cba8a7c70f75
Kent Watsen <kent@watsen.net>
2017-10-30 19:02:41 -0400

    this is -05

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,4 +1,4 @@
 
-pyang -p ../ -f tree --tree-line-length 71 --tree-print-groupings ../ietf-restconf-client\@*.yang > ietf-restconf-client-tree.txt
+pyang -p ../ -f tree --tree-line-length 71 ../ietf-restconf-client\@*.yang > ietf-restconf-client-tree.txt
 pyang -p ../ -f tree --tree-line-length 71 ../ietf-restconf-server\@*.yang > ietf-restconf-server-tree.txt
 
--- refs/validate-all.sh
+++ refs/validate-all.sh
@@ -3,23 +3,14 @@ echo "Testing YANG syntax..."
 pyang --ietf --max-line-length=70 -p ../ ../ietf-restconf-client\@*.yang
 pyang --ietf --max-line-length=70 -p ../ ../ietf-restconf-server\@*.yang
 
-exit
+yanglint -p ../ ../ietf-restconf-client\@*.yang
+yanglint -p ../ ../ietf-restconf-server\@*.yang
 
-echo "Testing keystore module..."
-./validate.sh ietf-keystore\@*.yang ex-keystore.xml
-#./validate.sh ietf-keystore\@*.yang ex-keystore-rpc-gpk-restconf-json.xml
-#./validate.sh ietf-keystore\@*.yang ex-keystore-rpc-gcsr-netconf.xml
 
-echo "Testing ssh-server module..."
-./validate.sh ietf-ssh-server\@*.yang ex-ssh-server.xml
+echo "Testing ex-restconf-client.xml..."
+yanglint -m -p ../ -s ../ietf-restconf-client\@*.yang ex-restconf-client.xml ../../keystore/refs/ex-keystore.xml
 
-echo "Testing tls-server module..."
-./validate.sh ietf-tls-server\@*.yang ex-tls-server.xml
-
-echo "Testing netconf-server module..."
-./validate.sh ietf-netconf-server\@*.yang ex-netconf-server.xml
-
-echo "Testing restconf-server module..."
-./validate.sh ietf-restconf-server\@*.yang ex-restconf-server.xml
+echo "Testing ex-restconf-server.xml..."
+yanglint -m -p ../ -s ../ietf-restconf-server\@*.yang ietf-x509-cert-to-name@2014-12-10.yang ex-restconf-server.xml ../../keystore/refs/ex-keystore.xml
 
 
