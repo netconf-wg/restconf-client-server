15f1909426c7bd71e00d81041293fe5dcb2d3e19
kwatsen <kent@watsen.net>
2021-12-14 14:10:16 -0500

    nits and what not

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -3,8 +3,8 @@ echo "Generating tree diagrams..."
 pyang -p ../ -f tree --tree-line-length 69 --tree-no-expand-uses ../ietf-restconf-client\@*.yang > ietf-restconf-client-tree-no-expand.txt
 pyang -p ../ -f tree --tree-line-length 69 --tree-no-expand-uses ../ietf-restconf-server\@*.yang > ietf-restconf-server-tree-no-expand.txt 
 
-#pyang -p ../ -f tree --tree-line-length 69 ../ietf-restconf-client\@*.yang > ietf-restconf-client-tree.txt
-#pyang -p ../ -f tree --tree-line-length 69 ../ietf-restconf-server\@*.yang > ietf-restconf-server-tree.txt
+pyang -p ../ -f tree --tree-line-length 69 ../ietf-restconf-client\@*.yang > ietf-restconf-client-tree.txt
+pyang -p ../ -f tree --tree-line-length 69 ../ietf-restconf-server\@*.yang > ietf-restconf-server-tree.txt
 
 extract_grouping_with_params() {
   # $1 name of module
